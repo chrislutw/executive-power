f571b52d6f97b292f13704c2cb3a6c18fad8c4da
chrislutw <kshke0723@gmail.com>
2022-04-25 00:47:19 +0800

    新增全域設定

---
+++ global-bottom.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer class="flex p-2 right-0 bottom-0 left-0 justify-between absolute">
+    <div>
+      <emojione:rocket class="inline" />執行力 - 呂正中
+    </div>
+    <div>
+      <SlideCurrentNo /> /
+      <SlidesTotal />
+    </div>
+  </footer>
+</template>
